f22a9c75d8c0a67f2931ba72250d7db586324632
Vinicius Furtado <viniciustales90@gmail.com>
2025-11-20 21:59:50 -0300

    vendo

--- .next/static/chunks/src_app_page_tsx_7f6890c7._.js
+++ .next/static/chunks/src_app_page_tsx_7f6890c7._.js
@@ -1,8 +1,8 @@
 (globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
     script: typeof document === "object" ? document.currentScript : undefined,
     chunks: [
-  "static/chunks/node_modules_f67271e5._.js",
-  "static/chunks/src_d4b4c114._.js"
+  "static/chunks/src_2b4b5660._.js",
+  "static/chunks/node_modules_3be12885._.js"
 ],
     source: "dynamic"
 });
