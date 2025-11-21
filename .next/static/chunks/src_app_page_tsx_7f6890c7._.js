973e4be68b5674f741752f1d5e6eb5a7623632f4
Vinicius Furtado <viniciustales90@gmail.com>
2025-11-20 22:07:53 -0300

    vamo

--- .next/static/chunks/src_app_page_tsx_7f6890c7._.js
+++ .next/static/chunks/src_app_page_tsx_7f6890c7._.js
@@ -1,8 +1,8 @@
 (globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
     script: typeof document === "object" ? document.currentScript : undefined,
     chunks: [
-  "static/chunks/src_2b4b5660._.js",
-  "static/chunks/node_modules_3be12885._.js"
+  "static/chunks/src_a2eac536._.js",
+  "static/chunks/node_modules_4eace8b9._.js"
 ],
     source: "dynamic"
 });
